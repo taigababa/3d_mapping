ceeaf128081f84c39c80ecb3b2015cb96051db1d
Taiga Baba <taiga.ba2.ringo@gmail.com>
2019-12-04 15:40:36 +0900

    edit eel program for presentation

--- Graduate/eel_figure/mapping.py
+++ Graduate/eel_figure/mapping.py
@@ -28,7 +28,7 @@ y_range = 100
 z_range = 100
 
 #切り取るスライスの深さ
-depth = 55
+depth = 25
 
 
 
@@ -37,7 +37,7 @@ depth = 55
 #slideは0だと横方向(plot的に)
 #slideは1だと縦方向(負の値だと上に動く)
 map_front = Hi_PROJECTION.make_3D_array(y_range,x_range,z_range)
-Hi_PROJECTION.fill_3D_array_slide('IMG_0117_result.png',map_front,0,153,2,4,1,0)
+Hi_PROJECTION.fill_3D_array_slide('IMG_8811_result.png',map_front,0,153,2,4,1,0)
 map_front = SLIDE.slide(map_front,-1,1)
 
 #Hi_PROJECTION.fill_3D_array_slide('IMG_0101_result.png',map_side,thresh,dist,mode,slide_dist,slide_mode,deg)
@@ -45,7 +45,7 @@ map_front = SLIDE.slide(map_front,-1,1)
 #slideは0だと縦方向(正の値だと上に動く)
 #横作成
 map_side = Hi_PROJECTION.make_3D_array(y_range,z_range,x_range)
-Hi_PROJECTION.fill_3D_array_slide('IMG_0118_result.png',map_side,0,134,2,-5,1,0)
+Hi_PROJECTION.fill_3D_array_slide('IMG_8810_result.png',map_side,0,134,2,-5,1,0)
 map_side = SLIDE.slide(map_side,3,1)
 map_side = np.flip(map_side.transpose(0,2,1),1)
 
@@ -55,10 +55,10 @@ map_side = np.flip(map_side.transpose(0,2,1),1)
 #0で上下(負の値で下)
 #1で左右(負の値で右)
 map_upper = Hi_PROJECTION.make_3D_array(z_range,x_range,y_range)
-Hi_PROJECTION.fill_3D_array_slide('IMG_0114_result.png',map_upper,0,174,2,0,1,355)
+Hi_PROJECTION.fill_3D_array_slide('IMG_8808_result.png',map_upper,0,104,2,0,1,85)
 #ここでずれの調節
 map_upper = SLIDE.slide(map_upper,1,1)
-map_upper = SLIDE.slide(map_upper,-7,0)
+map_upper = SLIDE.slide(map_upper,-1,0)
 map_upper = np.flip(np.flip(np.flip(map_upper,2).transpose(2,0,1),0),2)
 #map_upper = map_upper.transpose(0,2,1)
 #map_upper = np.flip(map_upper,2)
@@ -78,11 +78,20 @@ map_check_upper = map_front_side+map_upper
 #ADD.show_3D_3color(map_add_front_side)
 print('x=',map_true.shape[1],' y=',map_true.shape[0], ' z=',map_true.shape[2])
 
+#ここでノイズならし
 map_true_filtered = FILTER.Gaussian_blur_3D(map_true,3,0.3)
-Show_COLOR.show_3D_color(map_true_filtered,'pink',1)
-#Show_COLOR.show_3D_color(map_front,'pink',1)
-#Show_COLOR.show_3D_color(map_side,'pink',1)
-#Show_COLOR.show_3D_color(map_upper,'pink',1)
+
+#ここから結果表示を整えるために軸入れ替え(x,z)
+map_true_filtered_toshow = np.flip(map_true_filtered.transpose(1,2,0),2)
+map_front_toshow = np.flip(map_front.transpose(1,2,0),2)
+map_side_toshow = np.flip(map_side.transpose(1,2,0),2)
+map_upper_toshow = np.flip(map_upper.transpose(1,2,0),2)
+
+
+Show_COLOR.show_3D_color(map_true_filtered_toshow,'pink',1)
+Show_COLOR.show_3D_color(map_front_toshow,'blue',1)
+Show_COLOR.show_3D_color(map_side_toshow,'blue',1)
+Show_COLOR.show_3D_color(map_upper_toshow,'blue',1)
 #Show_COLOR.show_3D_color(map_front_side,'pink',1)
 
 #CHECK.show_img(map_front[:,:,0])
@@ -103,6 +112,12 @@ slice_img_upper = map_upper[depth,:,:]
 
 overlap_checker = slice_img_upper + slice_img_front_side
 
+plt.imshow(slice_img_upper, cmap='gray',interpolation='bicubic')
+plt.grid(which='major',color='lime',linestyle='-')
+plt.grid(which='minor',color='lime',linestyle='-')
+# plt.xticks([]), plt.yticks([])  #目盛りをなくす
+plt.show()
+
 place_checker = map_true_filtered[depth,:,:]
 plt.imshow(overlap_checker, cmap='gray',interpolation='bicubic')
 plt.grid(which='major',color='lime',linestyle='-')
@@ -111,7 +126,7 @@ plt.grid(which='minor',color='lime',linestyle='-')
 plt.show()
 
 #測定用に0,100のグレー画像に変更
-ret, zslice = cv2.threshold(place_checker, 0.5, 255, cv2.THRESH_BINARY_INV)
+ret, zslice = cv2.threshold(place_checker, 0.5, 255, cv2.THRESH_BINARY)
 
 
-CHECK.show_img(zslice,"z_slice50")
+#CHECK.show_img(zslice,"z_slice50")
